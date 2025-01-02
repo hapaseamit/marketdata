2782185af3bca980242ff9702092d805d46368f1
hapaseamit <amit@hapase.com>
2025-01-03 00:24:06 +0530

    updateall

--- getdata.py
+++ getdata.py
@@ -10,207 +10,142 @@ import pandas as pd
 import requests
 
 
-def get_session(url, base_url, headers_ref):
-    """
-    This funcions returns a session object with cookies set.
-    """
-    session = requests.Session()
-    request = session.get(base_url, headers=headers_ref)
-    cookies = dict(request.cookies)
-    return session.get(url, headers=headers_ref, cookies=cookies)
+def process_data(record, month):
+    """Processing data from the record"""
+    volume = 0
+    buyorders = 0
+    sellorders = 0
+    for data in record["data"]:
+        if data.get("expiryDate").split("-")[1] == month:
+            volume = (
+                volume
+                + data.get("CE", {"totalTradedVolume": 0})["totalTradedVolume"]
+                + data.get("PE", {"totalTradedVolume": 0})["totalTradedVolume"]
+            )
+
+            buyorders = (
+                buyorders
+                + (data.get("CE", {"totalBuyQuantity": 0})["totalBuyQuantity"])
+                + (data.get("PE", {"totalSellQuantity": 0})["totalSellQuantity"])
+            )
+
+            sellorders = (
+                sellorders
+                + (data.get("CE", {"totalSellQuantity": 0})["totalSellQuantity"])
+                + (data.get("PE", {"totalBuyQuantity": 0})["totalBuyQuantity"])
+            )
+    return (volume, buyorders, sellorders)
 
 
-def create_csv(csv_columns, symbol):
-    """
-    This function creates a folder and a csv file if not already present.
-    """
-    folder_path = os.path.join(os.getcwd(), "history", symbol)
-    csv = os.path.join(folder_path, str(datetime.today().date()) + ".csv")
-
-    # Check if csv file exists. If don't then create one
-    if not os.path.exists(csv):
-        with open(csv, "w", encoding="utf-8") as f_name:
-            f_name.write(csv_columns)
-    return csv
-
-
-def write_csv(newline, csv):
-    """
-    This function writes a new line to a csv file.
-    """
-    with open(csv, "a", encoding="utf-8") as f_name:
-        f_name.write(newline)
-    df = pd.read_csv(csv, skip_blank_lines=True)
-    df_cleaned = df.dropna(how="all")
-    df_cleaned.to_csv(csv, index=False)
-    time.sleep(3)
-
-
-def check_timestamp(df, timestamp, csv):
-    """
-    This function checks if the timestamp already exists in the csv file.
-    """
-    if timestamp in df["time"].values:
-        df = df[df["time"] != timestamp]
-        df_cleaned = df.dropna(how="all")
-        df_cleaned.to_csv(csv, index=False)
-
-    df_cleaned = df.dropna(how="all")
-    df_cleaned.to_csv(csv, index=False)
-    time.sleep(1)
-
-
-def get_volume(base_url, rest_url, call_headers, csv_columns, symbol):
+def get_data(base_url, rest_url, call_headers, csv_columns, symbol, datatype):
     """
     This module contains code for processing volume data.
     """
-    csv = create_csv(csv_columns, symbol)
+    status = "Market is Closed"
+    today = datetime.today().date().strftime("%d-%b-%Y")
+    month = str(datetime.today().strftime("%b"))
+    folder_path = os.path.join(os.getcwd(), "history", symbol)
+    csv = os.path.join(folder_path, str(today) + ".csv")
 
     while (
         not datetime.strptime("15:35:00", "%H:%M:%S").time()
         <= datetime.now().time()
         <= datetime.strptime("15:40:00", "%H:%M:%S").time()
     ):
+        # set session
         try:
-            response = get_session(base_url + rest_url, base_url, call_headers)
+            session = requests.Session()
+            request = session.get(base_url, headers=call_headers)
+            cookies = dict(request.cookies)
+            response = session.get(
+                base_url + rest_url, headers=call_headers, cookies=cookies
+            )
         except Exception:
             print("Exception while fetching response")
             continue
 
         if response.status_code != 200:
-            print(response.status_code)
+            print(response.status_code, " ", symbol)
             continue
 
+        # Fetch record
         try:
-            record = json.loads(response.content)["records"]
+            if datatype == "volume":
+                record = json.loads(response.content)["records"]
+            else:
+                record = json.loads(response.content)
+                status = record["marketStatus"]["marketStatusMessage"]
             timestamp = record["timestamp"][12:17]
         except Exception:
-            print("Exception while fetching record")
+            print("Exception while fetching record for ", symbol)
+            continue
+
+        if status == "Market is Closed" or timestamp.split(" ")[0] != today:
             continue
 
         if record is None:
-            print("None record")
+            print("Record is None for ", symbol)
             continue
 
-        volume = 0
-        buyorders = 0
-        sellorders = 0
+        # Check if csv file exists. If don't then create one
+        if not os.path.exists(csv):
+            with open(csv, "w", encoding="utf-8") as f_name:
+                f_name.write(csv_columns)
 
+        # Process data
         try:
-            for data in record["data"]:
-
-                volume = (
-                    volume
-                    + (data.get("CE", {"totalTradedVolume": 0})["totalTradedVolume"])
-                    + (data.get("PE", {"totalTradedVolume": 0})["totalTradedVolume"])
+            df = pd.read_csv(csv, skip_blank_lines=True)
+            if datatype == "volume":
+                volume, buyorders, sellorders = process_data(record, month)
+                if volume < 0:
+                    continue
+                if volume in df[symbol].values:
+                    continue
+                newline = str(
+                    "\n"
+                    + str(volume)
+                    + ","
+                    + str(buyorders)
+                    + ","
+                    + str(sellorders)
+                    + ","
+                    + str(timestamp)
                 )
-
-                buyorders = (
-                    buyorders
-                    + (data.get("CE", {"totalBuyQuantity": 0})["totalBuyQuantity"])
-                    + (data.get("PE", {"totalSellQuantity": 0})["totalSellQuantity"])
-                )
-
-                sellorders = (
-                    sellorders
-                    + (data.get("CE", {"totalSellQuantity": 0})["totalSellQuantity"])
-                    + (data.get("PE", {"totalBuyQuantity": 0})["totalBuyQuantity"])
-                )
+            else:
+                turnover = 0
+                for data in record["data"]:
+                    turnover = turnover + int(data["totalTurnover"])
+                if turnover in df[symbol].values:
+                    continue
+                newline = str("\n" + str(turnover) + "," + str(timestamp))
         except Exception:
-            print("Exception while additioing values")
+            print("Exception while processing data for ", symbol)
             continue
 
-        if volume < 0:
-            continue
+        # check if timestamp is already present
+        if timestamp in df["time"].values:
+            df = df[df["time"] != timestamp]
+            df_cleaned = df.dropna(how="all")
+            df_cleaned.to_csv(csv, index=False)
 
+        df_cleaned = df.dropna(how="all")
+        df_cleaned.to_csv(csv, index=False)
+        time.sleep(1)
+
+        # Write data to csv
+        with open(csv, "a", encoding="utf-8") as f_name:
+            f_name.write(newline)
         df = pd.read_csv(csv, skip_blank_lines=True)
-        if volume in df[symbol].values:
-            continue
-
-        check_timestamp(df, timestamp, csv)
-        newline = str(
-            "\n"
-            + str(volume)
-            + ","
-            + str(buyorders)
-            + ","
-            + str(sellorders)
-            + ","
-            + str(timestamp),
-        )
-        write_csv(newline, csv)
-    print("Market Closed!")
-
-
-def get_turnover(base_url, rest_url, call_headers, csv_columns, symbol):
-    """
-    This module contains code for processing turnover data.
-    """
-    csv = create_csv(csv_columns, symbol)
-
-    while (
-        not datetime.strptime("15:35:00", "%H:%M:%S").time()
-        <= datetime.now().time()
-        <= datetime.strptime("15:40:00", "%H:%M:%S").time()
-    ):
-        try:
-            response = get_session(base_url + rest_url, base_url, call_headers)
-        except Exception:
-            print("Exception while fetching response")
-            continue
-
-        if response.status_code != 200:
-            print(response.status_code)
-            continue
-
-        try:
-            status = json.loads(response.content)["marketStatus"]["marketOpenOrClose"]
-        except Exception:
-            print("Exception while fetching market status")
-            continue
-
-        if status != "Open":
-            print(status)
-            continue
-
-        try:
-            record = json.loads(response.content)
-            timestamp = record["timestamp"][12:17]
-        except Exception:
-            print("Exception while fetching record")
-            continue
-
-        if record is None:
-            print("No record!")
-            continue
-
-        turnover = 0
-        try:
-            for data in record["data"]:
-                turnover = turnover + int(data["totalTurnover"])
-        except Exception:
-            print("Exception while adding turnover values")
-            continue
-
-        df = pd.read_csv(csv, skip_blank_lines=True)
-        if turnover in df[symbol].values:
-            continue
-
-        check_timestamp(df, timestamp, csv)
-        newline = str(
-            "\n" + str(turnover) + "," + str(timestamp),
-        )
-
-        write_csv(newline, csv)
+        df_cleaned = df.dropna(how="all")
+        df_cleaned.to_csv(csv, index=False)
+        time.sleep(3)
     print("Market Closed!")
 
 
 def main():
     """
-    Main function to start threads for fetching and processing
-    option chain data for Nifty and Bank Nifty and their futures
+    Main function to start threads for fetching and processing data.
     """
-
     headers = {
         "user-agent": (
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -232,28 +167,32 @@ def main():
     sub_urls = ["nse50_opt", "nifty_bank_opt", "nse50_fut", "nifty_bank_fut"]
     threads = []
 
+    # Turnover threads
     for folder, sub_url in zip(folders, sub_urls):
         thread = threading.Thread(
-            target=get_turnover,
+            target=get_data,
             args=(
                 website_name,
                 f"api/liveEquity-derivatives?index={sub_url}",
                 headers,
                 f"{folder},time",
                 folder,
+                "turnover",
             ),
         )
         threads.append(thread)
 
+    # Volume threads
     for sym in ["nifty", "banknifty"]:
         thread = threading.Thread(
-            target=get_volume,
+            target=get_data,
             args=(
                 website_name,
-                f"api/liveEquity-derivatives?index={sym.upper()}",
+                f"api/option-chain-indices?symbol={sym.upper()}",
                 headers,
                 f"{sym}volume,{sym}buyorders,{sym}sellorders,time",
                 f"{sym}volume",
+                "volume",
             ),
         )
         threads.append(thread)
